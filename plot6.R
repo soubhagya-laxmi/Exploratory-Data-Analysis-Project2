1c64fc129d2ec533a78010a1c1b0aec71e9332e7
soubhagya Laxmi tripathy <shubbi.tripathy@gmail.com>
2016-11-14 14:56:38 +0530

    Plot 6

--- plot6.R
+++ plot6.R
@@ -1,16 +1,22 @@
-setwd("E:/shubbi study/R program/Corsera/Exploratory Data Analysis/week 4/case study project")
+#Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California (fips == "06037").
+#Which city has seen greater changes over time in motor vehicle emissions?
+
+#load data 
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 head(NEI)
 
+#Subset data for Baltimore city, MD and Los Angeles,CA
 subsetNEI <- subset(NEI,(fips %in% "24510"|fips %in% "06037") & type %in% "ON-ROAD")
 
+#Calculate the total emision by year and fips
 aggregatedTotalByYearAndFips <- aggregate(Emissions ~ year + fips, subsetNEI, sum)
 
+# Name the fips for Baltimore city, MD and Los Angeles,CA
 aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
 aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
 
-
+# Create a ggplot
 png("plot6.png", width=1040, height=480)
 g <- ggplot(aggregatedTotalByYearAndFips, aes(factor(year), Emissions))
 g <- g + facet_grid(. ~ fips)
