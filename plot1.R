efef496f3466b40e32457ed17e1a9bb644150251
soubhagya Laxmi tripathy <shubbi.tripathy@gmail.com>
2016-11-14 14:43:39 +0530

    Plot 1

--- plot1.R
+++ plot1.R
@@ -1,13 +1,13 @@
-## Set path
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
+# for each of the years 1999, 2002, 2005, and 2008.
 
-setwd("E:/shubbi study/R program/Corsera/Exploratory Data Analysis/week 4/case study project")
+# Loading provided datasets - loading from local machine
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 head(NEI)
 
-# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
-# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
-# for each of the years 1999, 2002, 2005, and 2008.
+#Total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 Total_Emission <- aggregate(Emissions ~ year,NEI,FUN = sum)
 
 png('plot1.png')
